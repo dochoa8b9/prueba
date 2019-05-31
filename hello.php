7cd916132ea8b6aaab6d05121cb31f83d8fb32ef
dochoa8b9 <dochoa8b9@gmail.com>
2019-05-31 11:59:08 -0500

    Prueba portable git

--- hello.php
+++ hello.php
@@ -5,3 +5,4 @@
 </style>
 <h1>Bienvenido a prueba.</h1>
 <p>Esta es una prueba para git portable con UniServer Z</p>
+<img src="https://www.imagen.com.mx/assets/img/imagen_share.png" alt="ImagenPrueba">
